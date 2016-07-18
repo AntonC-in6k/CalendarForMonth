4e4a095dc8183a693062ba784d5d65d618001b85
employee <antonc@softwareplanet.uk.com>
2016-07-18 14:59:51 +0300

    refactored code

--- src/monthcalendar/HandleCommands.java
+++ src/monthcalendar/HandleCommands.java
@@ -1,51 +1,51 @@
-//package monthcalendar;
-//
-//import java.io.IOException;
-//import java.util.Scanner;
-//
-///**
-// * Created by employee on 7/18/16.
-// */
-//public class HandleCommands {
-//    private ButtonCommands buttonCommands;
-//
-//
-//    public HandleCommands() {
-//    }
-//
-//    public String handleCommand() throws IOException {
-//        buttonPress();
-//        useCommand();
-//        return generateOutput(monthPeriod.getMonths());
-//    }
-//
-//    private ButtonCommands useCommand(String command) throws IOException {
-//        switch (command) {
-//            case "d":
-//            this.monthPeriod = monthPeriod.next();
-//            break;
-//
-//            case A:
-//                this.monthPeriod = monthPeriod.previous();
-//                break;
-//
-//            case W: //увеличить период
-//                this.monthPeriod = monthPeriod.increase();
-//
-//            case S:
-//                this.monthPeriod = monthPeriod.decrease();
-//                break;
-//
-//            default:
-//                break;
-//        }
-//    }
-//
-//    protected String buttonPress() {
-//        String result;
-//        Scanner scanner = new Scanner(System.in);
-//        result = scanner.next();
-//        scanner.close();
-//        return result;
-//    }
-//}
+package monthcalendar;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Created by employee on 7/18/16.
+ */
+public class HandleCommands {
+    private ButtonCommands buttonCommands;
+
+
+    public HandleCommands() {
+    }
+
+    public ButtonCommands handleCommand(String command) throws IOException {
+        return useCommand(buttonPress());
+    }
+
+    protected String buttonPress() {
+        String result;
+        Scanner scanner = new Scanner(System.in);
+        result = scanner.next();
+        scanner.close();
+        return result;
+    }
+
+    private ButtonCommands useCommand(String command) throws IOException {
+        switch (command) {
+            case "d":
+                return ButtonCommands.MOVE_LEFT;
+
+            case "a":
+                return ButtonCommands.MOVE_LEFT;
+
+            case "w":
+                return  ButtonCommands.MOVE_UP;
+
+            case "s":
+                return  ButtonCommands.MOVE_DOWN;
+
+            case "e":
+                return ButtonCommands.EXIT;
+
+            default:
+                return ButtonCommands.WRONG_COMMAND;
+        }
+    }
+
+
+}
